c99f7adb8ce0ba09c5de02424fc90bd3d6baa7d0
fuzzice <fuzzice@gmail.com>
2013-11-10 22:23:33 -0800

    Fixed file name

--- YouCanDoIt/src/FileInputStreamYo.java
+++ YouCanDoIt/src/FileInputStreamYo.java
@@ -1,6 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class FileInputStreamYo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		String path = "/Users/Awesome/Desktop/screenshot";
 		File f = new File(path);
